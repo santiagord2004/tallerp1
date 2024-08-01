1d9a39bcac256d0d7f7201f053529f5793345e12
santiagord2004 <santiagord2004@DESKTOP-AAP7VK4.localdomain>
2024-08-01 11:35:02 -0500

    peliculas añadidas

--- moviereviewsproject/movie/views.py
+++ moviereviewsproject/movie/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
-    return render(request, 'home.html', {'name':'Santiago Rodríguez'})
+    #return render(request, 'home.html', {'name':'Santiago Rodríguez'})
+
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(tittle__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+  
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies': movies})
 
 def about(request):
     return render(request, 'about.html')
