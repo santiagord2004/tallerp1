647d6262379043c18698d989cb84f5c80a977d72
santiagord2004 <santiagord2004@DESKTOP-AAP7VK4.localdomain>
2024-08-01 10:41:23 -0500

    cambios en la funcion home

--- moviereviewsproject/movie/views.py
+++ moviereviewsproject/movie/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
-    return render(request, 'home.html', {'name':'Greg Lim'})
+    return render(request, 'home.html', {'name':'Santiago Rodríguez'})
 
 def about(request):
     return HttpResponse('<h1>Welcome to About Page</h1>')
