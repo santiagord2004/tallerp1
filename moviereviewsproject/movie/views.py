45e14c46a624fa4c71a453fc459d4237d11ea90a
santiagord2004 <santiagord2004@DESKTOP-AAP7VK4.localdomain>
2024-08-01 10:53:55 -0500

    cambio color del nombre

--- moviereviewsproject/movie/views.py
+++ moviereviewsproject/movie/views.py
@@ -6,4 +6,4 @@ def home(request):
     return render(request, 'home.html', {'name':'Santiago Rodríguez'})
 
 def about(request):
-    return HttpResponse('<h1>Welcome to About Page</h1>')
+    return render(request, 'about.html')
